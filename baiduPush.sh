680006296095bd9ed373f8ad59fc0c0a7e389089
苏龙(10014903) <SUL-A@glodon.com>
2023-02-04 13:03:44 +0800

    百度收录尝试

--- baiduPush.sh
+++ baiduPush.sh
@@ -6,6 +6,6 @@ set -e
 node utils/baiduPush.js https://blog.addai.cn
 
 # 百度链接推送
-curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=blog.addai.cn&token=vvbXYJ2qGDEoBbDa"
+curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://blog.addai.cn&token=vvbXYJ2qGDEoBbDa"
 
 rm -rf urls.txt # 删除文件
